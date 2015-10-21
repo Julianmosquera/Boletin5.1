71dceedf2d77600f4d935d1799a5073ffbfb9cb4
julianlinux <julianlinux@julianlinux-X555LA>
2015-10-21 10:39:36 +0200

    cambios

--- src/boletin5/pkg1/Boletin51.java
+++ src/boletin5/pkg1/Boletin51.java
@@ -12,16 +12,16 @@ public class Boletin51 {
     
     public static void main(String[] args) {
        Coche primerCoche=new Coche();
-       JOptionPane.showInputDialog(null,"La velocidad incial es: "+primerCoche.getVelocidade());
+       JOptionPane.showMessageDialog(null,"La velocidad incial es: "+primerCoche.getVelocidade());
        String acelerarrespuesta=JOptionPane.showInputDialog("Teclea la velocidad de aceleracion");
        primerCoche.acelerar(Integer.parseInt(acelerarrespuesta));
-       JOptionPane.showInputDialog(null,"La velocidad tras acelerar es de: "+primerCoche.getVelocidade());
+       JOptionPane.showMessageDialog(null,"La velocidad tras acelerar es de: "+primerCoche.getVelocidade());
        String frenarrespuesta=JOptionPane.showInputDialog("Inserte la velocdidad de frenado");
        primerCoche.frenar(Integer.parseInt(frenarrespuesta));
-       JOptionPane.showInputDialog("La velocidad tras frenar es de :"+primerCoche.getVelocidade());
+       JOptionPane.showMessageDialog(null,"La velocidad tras frenar es de :"+primerCoche.getVelocidade());
        
         
-        
+        //Corregido los shows
         
         
         
